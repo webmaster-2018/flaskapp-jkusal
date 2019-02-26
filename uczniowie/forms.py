21b50a5ca0f15326db68bb25a6cff7eeb1df85cb
jkusal <juskusal@gmail.com>
2019-02-26 10:57:41 +0100

    php

--- uczniowie/forms.py
+++ uczniowie/forms.py
@@ -11,5 +11,13 @@ blad1 = 'To pole jest wymagane'
 
 class KlasaForm(FlaskForm):
     id = HiddenField("Klasa id")
-    klasa = 
+    klasa = StringField('Nazwa klasy: ', validators=[Required(message="blad_1")])
+    rok_naboru = StringField('Rok naboru: ', validators=[Required(message="blad_1")])
+    rok_matury = StringField('Rok matury: ', validators=[Required(message="blad_1")])
     
+class UczenForm(FlaskForm):
+    id = HiddenField()
+    imie = StringField('Imię ucznia: ', validators=[Required(message="blad_1")])
+    nazwisko = StringField('Nazwisko ucznia: ', validators=[Required(message="blad_1")])
+    plec = SelectField('Płeć ucznia: ', coerce=int)
+    klasa = SelectField('Klasa: ', coerce=int)
